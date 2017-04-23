ba15eb5a91679f51016e10eefb48e219628e2497
Wicia <michalvietecha@gmail.com>
2017-04-23 23:12:55 +0200

    Missing files...

--- DisciplesBattleSimulator/src/units/attributes/impl/models/hitpoints/UnitHitPoints.java
+++ DisciplesBattleSimulator/src/units/attributes/impl/models/hitpoints/UnitHitPoints.java
@@ -1,38 +1,14 @@
-package units.attributes.impl.models;
+package units.attributes.impl.models.hitpoints;
 
 import units.attributes.impl.base.*;
 import units.attributes.api.Attribute;
 import units.attributes.api.AttributeValue;
-import units.attributes.modificators.impl.AttributeValueChangeImpl;
 import units.attributes.modificators.api.AttributeValueChange;
 
 public class UnitHitPoints extends AbstractAttribute implements Attribute{
     
     public static final String CODE = "hitPoints";
     public static final int DEFAULT_HIT_POINTS = 1;
-
-    private enum DeltaFactor{
-        
-        ARMOR(UnitArmor.CODE, 0.6), 
-        DAMAGE(UnitDamage.CODE, 1.0);
-        
-        private final String attributeCode;
-        private final double conversionValue;
-
-        private DeltaFactor(String attributeCode, double conversionValue) {
-            this.attributeCode = attributeCode;
-            this.conversionValue = conversionValue;
-        }
-
-        public String getAttributeId() {
-            return attributeCode;
-        }
-
-        private AttributeValueChange getAttributeChange(double attributeChangeFactor) {
-            double finalfactor = conversionValue * attributeChangeFactor;
-            return new AttributeValueChangeImpl(finalfactor);
-        }
-    }
     
     public UnitHitPoints() {
         this(new NumericValue(DEFAULT_HIT_POINTS));
@@ -42,25 +18,26 @@ public class UnitHitPoints extends AbstractAttribute implements Attribute{
         super(CODE, value);
     }
 
-    //TODO: !!! gdzie powinien byc widoczny update atrybutu w zbiorze? !!!
     @Override
     public void updateValue(AttributeValueChange change, UnitAttributes attributes) {
-        AbstractAttribute result = change.apply(this);
+        AttributeValue newValue = change.getNewValue(this);
+        this.setValue(newValue);
         this.updateReferencedAttributes(attributes);
     }
     
     private void updateReferencedAttributes(UnitAttributes attributes){
         AbstractAttribute maxHpAttribute = attributes.getAttributeById(UnitMaxHitPoints.CODE);
         double hitPointsChangeFactor = getHitPointsChangeFactor(maxHpAttribute);
-        for(DeltaFactor factor : DeltaFactor.values()){
-            AbstractAttribute attribute = attributes.getAttributeById(factor.attributeCode);
+        for(LinkedAttributesDelta factor : LinkedAttributesDelta.values()){
+            AbstractAttribute attribute = attributes.getAttributeById(factor.getAttributeId());
             AttributeValueChange attributeChange = factor.getAttributeChange(hitPointsChangeFactor);
-            attributeChange.apply(attribute);
+            AttributeValue newValue = attributeChange.getNewValue(attribute);
+            attribute.setValue(newValue);
         }
     }
     
-    private double getHitPointsChangeFactor(AbstractAttribute attribute){
-        AttributeValue<Integer> maxHpValue = attribute.getValue();
+    private double getHitPointsChangeFactor(AbstractAttribute maxHitPointsAttribute){
+        AttributeValue<Integer> maxHpValue = maxHitPointsAttribute.getValue();
         int maxHp = maxHpValue.value();
         int curentHitPoints = this.getSimpleValue();
         double factor = (curentHitPoints * 100.0) / (maxHp * 1.0);
